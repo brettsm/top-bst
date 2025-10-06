ed033673743649057955ee3576ac30fb07b0f67d
Brett <brettdansmith@gmail.com>
2025-10-05 20:09:27 -0400

    Complete iterative buildTree(arr)

--- datastructure/binarySearchTree.js
+++ datastructure/binarySearchTree.js
@@ -1,3 +1,5 @@
+import { Queue } from "./queue.js";
+
 export class Tree {
     constructor(arr) {
         let cleaned = removeDuplicates(mergeSort(arr));
@@ -5,16 +7,40 @@ export class Tree {
     }
 
     buildTree(arr) {
-        let mid = Math.floor(arr.length / 2);
+        if (arr.length < 1) return null;
+        const mid = Math.floor((arr.length - 1) / 2);
+
+        const q = new Queue();
         const root = new Node(arr[mid]);
 
-        let left = Math.floor(arr.splice(0,mid).length);
-        console.log(left);
+        q.enqueue({n: root, s: 0, e: arr.length - 1});
+        while (!q.isEmpty()) {
+            let curr = q.dequeue();
+            let node = curr.n;
+            let start = curr.s;
+            let end = curr.e;
+            let index = start + Math.floor((end - start) / 2);
 
+            if (start < index) {
+                let midLeft = start + Math.floor((index - 1 - start) / 2); // middle of the left side of array, excluding the mid value
+                let left = new Node(arr[midLeft]);
+                node.left = left;
+                q.enqueue({n: left, s: start, e: index - 1}); // now we need to find the left and right children 'left' if they exist so push it in
+            }
+
+            if (index < end) { 
+                let midRight = index + 1 + Math.floor((end - index - 1) / 2);  // (end - (index + 1)) ==> (end - index - 1)
+                let right = new Node(arr[midRight]);
+                node.right = right;
+                q.enqueue({n: right, s: index + 1, e: end});
+            }
+        }
+        
         return root;
+
     }
 }
-
+ 
 class Node {
     constructor(value) {
         this.left = null;
