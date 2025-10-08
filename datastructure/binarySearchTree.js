fb4d01ccc19bec82b29f8cb8b42203e46271016f
Brett <brettdansmith@gmail.com>
2025-10-08 11:53:15 -0400

    Create printTree function to display the tree

--- datastructure/binarySearchTree.js
+++ datastructure/binarySearchTree.js
@@ -37,9 +37,33 @@ export class Tree {
         }
         
         return root;
+    }
 
+    printTree(node, prefix = '', isLeft = true) {
+        if (node === null) return;
+
+        if (node.right !== null) {
+            printTree(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        }
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+        if (node.left !== null) {
+            printTree(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+        }
     }
 }
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
  
 class Node {
     constructor(value) {
